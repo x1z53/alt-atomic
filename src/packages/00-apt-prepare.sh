c96b97381da1a4dd59f9a8af415e12fe7ab33e96
x1z53 <x1z53@.>
2025-04-06 11:17:25 +0300

    Пакеты

--- src/packages/00-apt-prepare.sh
+++ src/packages/00-apt-prepare.sh
@@ -2,6 +2,8 @@
 
 echo "::group:: ===$(basename "$0")==="
 
+echo "rpm http://ftp.aides.space/repo/altlinux/Sisyphus x86_64 aides" >> /etc/apt/sources.list.d/alt.list
+
 apt-get update
 
 echo "::endgroup::"
--- src/packages/01-package-list.sh
+++ src/packages/01-package-list.sh
@@ -4,11 +4,55 @@ set -euo pipefail
 echo "::group:: ===$(basename "$0")==="
 
 BASIC_PACKAGES=(
-  systemd-ssh-agent
-  ptyxis
+  kitty
+  telegram-desktop
+  pinentry-gnome3
+  keyd
+  showtime
+  cassette
+  gdb
+  neovim
+  npm
+  fish
+  zoxide
+  eza
+  yandex-browser-stable
+  apt-repo
+  alt-gnome-desktop-wallpapers
+  xclip
+  papers
+  i586-fasm
+  fzf
+  ripgrep
+  hasher
+  gear
+  btop
+  apa
+  gear-remotes-utils
+  hasher-priv
+  texlive-dist
+  rnote
+  qucs-s
+  rpm-utils
+  alt-gpgkeys
+  thunderbird
 )
 
-apt-get install -y \
-  "${BASIC_PACKAGES[@]}" \
+apt-get install -y "${BASIC_PACKAGES[@]}"
+
+PACKAGES_TO_REMOVE=(
+  ptyxis
+  gnome-console
+  gnome-tour
+  clapper
+  yelp
+  gnome-maps
+  gucharmap
+  file-roller
+  gnome-shell-extensions
+  firefox
+)
+
+apt-get remove -y "${PACKAGES_TO_REMOVE[@]}"
 
 echo "::endgroup::"
